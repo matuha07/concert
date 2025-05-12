5799a04ff4ed2ba27fa28e5d189f46ae58455a38
Matvey Terekhov <kosoklaz@gmail.com>
2025-05-12 14:58:50 +0500

    inital commit

---
+++ src/components/shared/header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface Props {
+    classname?: string;
+}
+
+export const Header:React.FC<Props> = ({ className }) => {
+    return (
+        <div></div>
+    )
+}
